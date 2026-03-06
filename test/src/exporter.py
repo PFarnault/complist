9d8689de63303029fe2e8c0c698092e3d209027c
Farnault Paul <paul.farnault@sciencespo.fr>
2026-03-06 13:24:56 +0100

    update code and remove unecessary files

--- test/src/exporter.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-def export_to_excel(df: pd.DataFrame, out_path: Path):
-    df.to_excel(out_path)
-
-def export_to_csv(df: pd.DataFrame, out_path: Path):
-    df.to_csv(out_path)
